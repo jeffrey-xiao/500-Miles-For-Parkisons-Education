3ca8de2eed012c5fe87a3c7a8b40916e2d8bc70c
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-04-03 14:50:33 -0400

    Added 404 page and blog

--- app.js
+++ app.js
@@ -10,23 +10,46 @@ var port = 3000;
 var hostname = 'localhost';
 
 var srcPath = __dirname;
-var destPath = path.join(__dirname, 'public');
+var destPath = __dirname + '/public';
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
 app.use(sassMiddleware({
 	src: srcPath,
 	dest: destPath,
 	debug: true,
+	force: true,
 	outputStyle: 'expanded'
 }));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function (req, res) {
 	res.render('index', {
 		title: 'Home'
+	});
+});
+
+app.get('/blog/:name', function (req, res) {
+	res.render('blog/' + req.params.name, {
+		title: req.params.name
+	}, function (err, result) {
+		if (err) {
+			console.log(err);
+			res.render('404', {
+				title: 'Page Not Found'
+			});
+		} else {
+			res.end(result);
+		}
+	});
+});
+
+app.get('*', function (req, res) {
+	res.render('404', {
+		title: 'Page Not Found'
 	});
 });
 
